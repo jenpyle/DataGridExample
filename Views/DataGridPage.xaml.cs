1bcc94541e2061bc3408af39837ced1be7dcab4d
Pyle <jennifer.pyle@thermofisher.com>
2022-10-28 19:33:36 -0600

    Add animation and refresh animation

--- Views/DataGridPage.xaml.cs
+++ Views/DataGridPage.xaml.cs
@@ -5,8 +5,10 @@ using System.Runtime.CompilerServices;
 
 using DataGridAnimation.Core.Models;
 using DataGridAnimation.Core.Services;
+using Microsoft.Toolkit.Uwp.UI;
 using Microsoft.Toolkit.Uwp.UI.Controls;
 using Microsoft.Toolkit.Uwp.UI.Controls.Primitives;
+using Microsoft.Toolkit.Uwp.UI.Helpers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
@@ -16,7 +18,17 @@ namespace DataGridAnimation.Views
 {
     public sealed partial class DataGridPage : Page, INotifyPropertyChanged
     {
-        public SelectableObservableCollection<SampleOrder> Source { get; } = new SelectableObservableCollection<SampleOrder>();
+        public SelectableObservableCollection<SampleOrder> Source { get; set; } = new SelectableObservableCollection<SampleOrder>();
+        public ObservableCollection<SampleOrder> Backup { get; set; } = new ObservableCollection<SampleOrder>();
+
+        public ObservableCollection<SampleOrder> SourceDP
+        {
+            get { return (ObservableCollection<SampleOrder>)GetValue(SourceDPProperty); }
+            set { this.SetValue(SourceDPProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceDPProperty = DependencyProperty.Register(
+            nameof(SourceDP), typeof(ObservableCollection<SampleOrder>), typeof(DataGridPage), new PropertyMetadata(default(ObservableCollection<SampleOrder>)));
 
         // TODO: Change the grid as appropriate to your app, adjust the column definitions on DataGridPage.xaml.
         // For more details see the documentation at https://docs.microsoft.com/windows/communitytoolkit/controls/datagrid
@@ -24,19 +36,24 @@ namespace DataGridAnimation.Views
         {
             InitializeComponent();
             this.DataContext = this;
+            SourceDP = new ObservableCollection<SampleOrder>();
         }
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
             Source.Clear();
+            Backup = new ObservableCollection<SampleOrder>(Source);
+            SourceDP.Clear();
 
             // Replace this with your actual data
             var data = await SampleDataService.GetGridDataAsync();
 
             foreach (var item in data)
             {
+                SourceDP.Add(item);
                 Source.Add(item);
+                Backup = new ObservableCollection<SampleOrder>(Source);
             }
         }
 
@@ -76,6 +93,9 @@ namespace DataGridAnimation.Views
             Func<SampleOrder, IComparable> keySelector = GetKeySelector(columnTag);
 
             Source.Sort(keySelector, listSortDirection);
+            Backup = new ObservableCollection<SampleOrder>(Source);
+            //Backup.Sort(keySelector, listSortDirection);
+            SourceDP.Sort(keySelector, listSortDirection);
 
             ResetColumns(dataGrid, columnTag, e.Column.SortDirection);
         }
@@ -138,6 +158,8 @@ namespace DataGridAnimation.Views
             newOrder.ShipTo = "Beverly Hills, CA";
             newOrder.Company = "Thermo Fisher Scientific";
             Source.Add(newOrder);
+            Backup = new ObservableCollection<SampleOrder>(Source);
+            SourceDP.Add(newOrder);
         }
 
         public int PresenterHeight
@@ -165,9 +187,77 @@ namespace DataGridAnimation.Views
             ToggleValue = !ToggleValue;
         }
 
+        public static T FindControl<T>(UIElement parent, Type targetType, string ControlName) where T : FrameworkElement
+        {
+            if (parent == null) return null;
+
+            if (parent.GetType() == targetType && ((T)parent).Name == ControlName)
+            {
+                return (T)parent;
+            }
+            T result = null;
+            int count = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < count; i++)
+            {
+                UIElement child = (UIElement)VisualTreeHelper.GetChild(parent, i);
+
+                if (FindControl<T>(child, targetType, ControlName) != null)
+                {
+                    result = FindControl<T>(child, targetType, ControlName);
+                    break;
+                }
+            }
+            return result;
+        }
+
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             Source.RemoveAt(0);
+
+            SourceDP.RemoveAt(0);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            DataGridCellsPresenter dataGridCellsPresenter = FindControl<DataGridCellsPresenter>(this, typeof(DataGridCellsPresenter), "CellsPresenter");
+
+            Source.Clear();
+            SourceDP.Clear();
+
+            Backup.Clear();
+            //Source.UpdateContents(Source);
+            //Backup.UpdateContents(Source);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Source.UpdateContents(Source);
+            Backup = new ObservableCollection<SampleOrder>();
+            Backup = new ObservableCollection<SampleOrder>(Source);
+            SourceDP.UpdateContents(SourceDP);
+        }
+
+        public async void RefillList()
+        {
+            var data = await SampleDataService.GetGridDataAsync();
+
+            foreach (var item in data)
+            {
+                SourceDP.Add(item);
+                Source.Add(item);
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            RefillList();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            Source.UpdateContents(Source);
+            SourceDP = new ObservableCollection<SampleOrder>();
+            SourceDP.UpdateContents(Source);
         }
     }
 }
